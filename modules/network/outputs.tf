6876aed5dfccec956ed84c8c68e2f8dfdcfc6567
Raghu K <raghuk.vit@gmail.com>
2026-05-05 08:48:32 +0530

    WMP scripts

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,12 +1,8 @@
-output "subnet_ids" {
-  value = [for i, j in aws_subnet.main : j.id]
-}
-
 output "vpc_id" {
   value = aws_vpc.main
 }
 
-output "subnets_merged" {
-  value = local.subnets_with_igw
+output "subnet_ids" {
+  value = aws_subnet.main
 }
 
