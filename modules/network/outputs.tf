9561de6253b9a498fb9d9a18c99cddc235e1e5c6
Raghu K <raghuk.vit@gmail.com>
2026-05-05 08:58:11 +0530

    WMP scripts

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -6,3 +6,7 @@ output "db_subnet_ids" {
   value = [ for i,j in local.subnets_merged: j.id if j.group == "db" ]
 }
 
+output "app_subnet_ids" {
+  value = [ for i,j in local.subnets_merged: j.id if j.group == "app" ]
+}
+
