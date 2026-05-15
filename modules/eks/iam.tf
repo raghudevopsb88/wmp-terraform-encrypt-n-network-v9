5e60f0899ad3b9be478f88569355b1e9f0705323
Raghu K <raghuk.vit@gmail.com>
2026-05-15 08:54:54 +0530

    terraform: update 1 file(s) (iam.tf)

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -55,3 +55,68 @@ resource "aws_eks_pod_identity_association" "external-dns-pod-association" {
   role_arn        = aws_iam_role.external-dns-role.arn
 }
 
+
+resource "aws_iam_role" "aws-ssm-ps-role" {
+  name = "aws-ssm-ps-role-${var.env}"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "pods.eks.amazonaws.com"
+        },
+        "Action" : [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "aws-ssm-ps-role-${var.env}"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : [
+            "*"
+          ]
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_eks_pod_identity_association" "aws-ssm-ps-role-pod-association-analytics-service" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "default"
+  service_account = "analytics-service"
+  role_arn        = aws_iam_role.aws-ssm-ps-role.arn
+}
+
+resource "aws_eks_pod_identity_association" "aws-ssm-ps-role-pod-association-auth-service" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "default"
+  service_account = "auth-service"
+  role_arn        = aws_iam_role.aws-ssm-ps-role.arn
+}
+
+resource "aws_eks_pod_identity_association" "aws-ssm-ps-role-pod-association-portfolio-service" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "default"
+  service_account = "portfolio-service"
+  role_arn        = aws_iam_role.aws-ssm-ps-role.arn
+}
+
